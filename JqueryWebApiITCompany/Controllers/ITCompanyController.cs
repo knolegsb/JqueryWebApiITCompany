b507ccd67b02e4d65a7feb92699b6bec7da9005c
Sean <seon2you@hotmail.com>
2016-11-15 19:01:38 -0800

    change main to itcompany

---
+++ JqueryWebApiITCompany/Controllers/ITCompanyController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JqueryWebApiITCompany.Controllers
+{
+    public class ITCompanyController : Controller
+    {
+        // GET: ITCompany
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
